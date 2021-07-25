3b0d1834f59e43e33afa8445e0e9e749e13e5e37
Cory <corys4139@gmail.com>
2021-07-25 14:22:32 -0400

    add axios and link up front and back ends

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+  const testFunction = () => {
+    axios
+			.get("/users/")
+			.then((response) => console.log("server response", response.data));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cory rules</h1>
+      <div>
+        <button onClick={testFunction}>Test</button>
+      </div>
     </div>
   );
 }
